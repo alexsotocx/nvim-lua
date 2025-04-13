2e2d4ad3a537fe4bb6164cfde627cec1a6fad560
Alex Soto <alexsotocx@gmail.com>
2025-04-13 10:51:05 +0200

    latest update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- transform previous mapping to use map function
+map("n", "<leader>tn", "<cmd>lua require('neotest').run.run()<CR>", { desc = "Run nearest test" })
+map("n", "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", { desc = "Run file tests" })
+map("n", "<leader>to", "<cmd>lua require('neotest').output.open()<CR>", { desc = "Open test output" })
+map("n", "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<CR>", { desc = "Toggle test summary" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,31 +12,56 @@ return {
       require "configs.lspconfig"
     end,
   },
-{
-  'mrcjkb/rustaceanvim',
-  version = 'v5.15.0', -- Recommended
-  lazy = false, -- This plugin is already lazy
-},
-{
+  {
+    "mrcjkb/rustaceanvim",
+    version = "v5.26.0", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "marilari88/neotest-vitest", -- Vitest adapter for Neotest
+    },
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require "neotest-vitest",
+        },
+      }
+    end,
+  },
+  {
     "kdheepak/lazygit.nvim",
     lazy = true,
     cmd = {
-        "LazyGit",
-        "LazyGitConfig",
-        "LazyGitCurrentFile",
-        "LazyGitFilter",
-        "LazyGitFilterCurrentFile",
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
     },
     -- optional for floating window border decoration
     dependencies = {
-        "nvim-lua/plenary.nvim",
+      "nvim-lua/plenary.nvim",
     },
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
     keys = {
-        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
-},
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
+    "github/copilot.vim",
+    lazy = true,
+    cmd = {
+      "Copilot",
+    },
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
